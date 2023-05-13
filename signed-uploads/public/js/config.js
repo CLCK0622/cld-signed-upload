c7bc6196071378b5bf77ce94673e6a752f9768da
钟熠 <zhongyi070622@163.com>
2023-05-13 22:18:11 +0800

    Update config.js

--- signed-uploads/public/js/config.js
+++ signed-uploads/public/js/config.js
@@ -3,10 +3,12 @@ const cloudinary = require('cloudinary').v2;
 // Configure your cloud name, API key and API secret:
 
 const myconfig = cloudinary.config({
-  cloud_name: '<your_cloud_name>',
-  api_key: '<your_api_key>',
-  api_secret: '<your_api_secret>',
-  secure: true
+    cloud_name: "dat4mqsnf",
+    api_key: "699686925639313",
+    api_secret: "7H747SmwNDMPbqxPXR0pLjtqa9I",
+    upload_preset: "upload2022",
+    secure: true
 });
 
 exports.myconfig = myconfig;
+
